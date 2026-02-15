6a9af80ef0ea481a238ba55379fe7bf6f1c850ea
ricardo007 <ricardo.m.barros6@aluno.senai.br>
2026-02-15 13:14:03 -0300

    .

---
+++ exercicios/Dobro_Triplo_RaizQuadrada.py
@@ -0,0 +1,7 @@
+print("Dobro, Triplo, Raiz Quadrada")
+
+print("")
+print("")
+
+numero = int(input("Digite um numero: "))
+print("O dobro de {} é {}, Triplo de {} é {} e a raiz quadrada de {} é {:.2f}".format(numero, numero* 2, numero, numero* 3, numero, numero ** 0.5))
---
+++ exercicios/antecessor_sucessor.py
@@ -0,0 +1,8 @@
+numero = int(input("Digite um numero para descobrir seu ancessor e sucessor: "))
+
+antecessor = numero - 1
+sucessor = numero + 1
+
+print("O antecessor de {} é {} e o sucessor é {}".format(numero, antecessor, sucessor))
+
+
