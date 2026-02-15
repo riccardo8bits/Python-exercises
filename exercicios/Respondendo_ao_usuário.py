1651c4f2a5c890e6e870d5ecdbd4aaa275b7146f
ricardo007 <ricardo.m.barros6@aluno.senai.br>
2026-02-15 12:18:56 -0300

    first commit

---
+++ exercicios/Respondendo_ao_usuário.py
@@ -0,0 +1,4 @@
+programa = input("What's your name? ") #input para perguntar e salvo em uma variável
+
+print("Nice to meet you, {}!!!".format(programa)) #.format, eu troco as chaves dentro do print pela variavel que está dentro do format
+
---
+++ exercicios/print.py
@@ -0,0 +1,18 @@
+print("")
+print("")
+
+
+print("Olá, Mundo!")
+
+
+
+print("")
+print("")
+
+
+msg = "Hello world" #Guardei na variável
+print(msg) #puxei a variável "msg" e printei
+
+
+print("")
+print("")
