15285b7fd693b60b9dce408c11da4fb2b13871be
ricardo007 <ricardo.m.barros6@aluno.senai.br>
2026-02-17 13:04:41 -0300

    new

---
+++ exercicios/media_aritimetica.py
@@ -0,0 +1,8 @@
+#média de duas notas de um aluno
+
+nota1 = float(input("Digite a primeira nota do aluno: "))
+nota2 = float(input("Digite a segunda nota do aluno: "))
+
+media = (nota1+nota2) / 2
+
+print("A média desse aluno foi {:.1f}".format(media))
